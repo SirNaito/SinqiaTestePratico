996a2147838bd2b0031e4f5f1726ea78ca864606
SirNaito <gabrielcabral22@hotmail.com>
2025-03-12 01:24:03 -0300

    Revisão Final do modelo Basico

--- BibliotecaCategorias/Category.cs
+++ BibliotecaCategorias/Category.cs
@@ -98,6 +98,7 @@ namespace BibliotecaCategorias
                 }
 
                 Console.WriteLine(category ?? "Unknown Category");
+
             }
         }
     }
--- SinqiaTestePratico/Program.cs
+++ SinqiaTestePratico/Program.cs
@@ -74,6 +74,7 @@ namespace TesteRisco
                 {
                     exitapp = true;
                 }
+
             }
         }
     }
